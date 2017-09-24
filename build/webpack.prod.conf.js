aff0da9c47f275bf01248b2bae919d759c8c2569
otfngo <otfngo@qq.com>
2017-09-24 16:13:45 +0800

    ...

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -42,6 +42,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     new ExtractTextPlugin({
       filename: utils.assetsPath('css/[name].[contenthash].css')
     }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
